d4467c519970c59fb4ffaedc8def296d579721d2
Rafael mosias <rafael.mosias@iclinic.com.br>
2020-01-07 18:01:19 -0300

    fix: add s word

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -22,7 +22,7 @@ export default function SimpleBackdrop() {
           setOpen(!open);
         }}
       >
-        Show backdrop
+        Show backdrops
       </button>
       <Backdrop
         className={classes.backdrop}
